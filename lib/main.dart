820ef94eeaa9261c22fe84d1ef3e5f10a1f1ed44
Achudh Krishna <achudhk@gmail.com>
2022-04-23 13:46:14 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,156 +1,89 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-      Widget titleSection = Container(
-  padding: const EdgeInsets.all(32),
-  child: Row(
-    children: [
-      Expanded(
-        /*1*/
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            /*2*/
-            Container(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: Text(
-                'Oeschinen Lake Campground',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Text(
-              'Kandersteg, Switzerland',
-              style: TextStyle(
-                color: Colors.grey[500],
-              ),
-            ),
-          ],
-        ),
-      ),
-      /*3*/
-       FavoriteWidget(),
-    ],
-  ),
-);
-Color color = Theme.of(context).primaryColor;
-
-Widget buttonSection = Container(
-  child: Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: [
-      _buildButtonColumn(color, Icons.call, 'CALL'),
-      _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-      _buildButtonColumn(color, Icons.share, 'SHARE'),
-    ],
-  ),
-);
-    Widget textSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-            'Alps. Situated 1,578 meters above sea level, it is one of the '
-            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-            'half-hour walk through pastures and pine forest, leads you to the '
-            'lake, which warms to 20 degrees Celsius in the summer. Activities '
-            'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
-      ),
-    );
     return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter layout demo'),
-        ),
-        body: ListView(
-          children: [
-            Image.asset(            
-                    'images/lake.jpg',
-                    width: 600,
-                    height: 240,
-                    fit: BoxFit.cover,
-                    ),
-            titleSection,
-            buttonSection,
-            textSection,
-          ]
-        ),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    );
-  }
-  
-    Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
-          ),
-        ),
-      ],
+      home: const MyWidget(),
     );
   }
 }
-  class FavoriteWidget extends StatefulWidget {
-    FavoriteWidget({Key key}) : super(key: key);
-  
-    @override
-    _FavoriteWidgetState createState() => _FavoriteWidgetState();
-  }
-  
-  class _FavoriteWidgetState extends State<FavoriteWidget> {
-    bool _isFavorited = true;
-    int _favoriteCount = 41;
-    void _toggleFavorite (){
-        setState(() {
-          if(_isFavorited){
-            _favoriteCount -=1;
-            _isFavorited = false;
-          }
-          else{
-            _favoriteCount +=1;
-            _isFavorited = true;
-          }
-        });
-      }
-    @override
-    Widget build(BuildContext context) {
-      return Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [Container(
-                padding: EdgeInsets.all(0),
-                child: IconButton(
-                icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
-                color: Colors.red[500],
-                onPressed: _toggleFavorite,
-                ),
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Flutter layout demo",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Image(
+              image: const AssetImage(
+                "images/lake.jpeg",
               ),
-              SizedBox(
-                width: 18,
-                child: Container(child: Text('$_favoriteCount'),)
-              )
-              ]
-            );
-    }
-
-
+              fit: BoxFit.cover,
+              height: MediaQuery.of(context).size.height * 0.8,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const <Widget>[
+                          Text(
+                            "Heading text",
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "Sub-heading text",
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            "Sub-heading text",
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
